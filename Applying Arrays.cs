dae02d968416889697b863fdcb36b5219ee28227
JohnDamiano <82375854+JohnDamiano@users.noreply.github.com>
2021-05-05 12:51:11 +0800

    Update Applying Arrays.cs

--- Applying Arrays.cs
+++ Applying Arrays.cs
@@ -3,44 +3,61 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace TeamTaskManager
+namespace CSharp_Shell
 {
 
     public class Program 
     {
         public static void Main()
         {
-			
-		Console.WriteLine("Team Task Manager");
-     string Member1name = "John A. nhel Damiano";
+		
+        
+     Console.WriteLine("Team Task Manager");
+     Console.WriteLine("");
+     
+     string Member1name = "John A. Nhel Damiano";
      string Project = "Team Task Management System";
      string Assignment = "Debugger";
+     string Username = "JND20";
      string password = "Damiano";
      string possition = "Team leader";
-    
-    Console.Write("Enter Your Password:");
-    String Passwordinput = Console.ReadLine();
-    if (Passwordinput  ==  password)
-    {
-    	Console.WriteLine("Name:"+Member1name);
+            
+     int i = 0;
+     for (; i < 2; i++)
+     {
+     	
+     Console.Write("Enter Username:");
+     string usernameInput = Console.ReadLine();
 
-        Console.WriteLine("Project:"+Project);
+     Console.Write("Enter Password:");
+     string passwordInput = Console.ReadLine();
 
-        Console.WriteLine("Possition:"+possition);
-    	Console.WriteLine("Assignment:"+Assignment);
-    
-    	
-    	
-    	}
-    else
-    {
-    	
-       Console.WriteLine("Wrong password try again! ");
-      
-    }
-   {
-   	 string[]Task={"Encoder: ","Finance: ","Editor: "};
-   	 string[] members =new string[3];
+     Console.WriteLine();
+     if (usernameInput == Username && passwordInput == password)
+     {
+     	
+     Console.WriteLine("Member's INFORMATION");
+     Console.WriteLine("Name:" + Member1name);
+     Console.WriteLine("Project:" + Project);
+     Console.WriteLine("Task: " + Assignment);
+     Console.WriteLine("Possition:" + possition);
+     break;
+     
+      }
+      else
+      {
+      Console.WriteLine("Wrong User name or password Try again!");
+      Console.WriteLine("");
+      }
+      }
+      if (i == 2)
+      {
+      Console.WriteLine("Application will exit...");
+      Environment.Exit(0);
+           
+      }	
+     var Task =new string[3];
+   	 var members =new string[3];
      	
    	 Console.WriteLine("");
      Console.WriteLine("");
@@ -51,21 +68,27 @@ namespace TeamTaskManager
      members[2] = Console.ReadLine();
      
      Console.WriteLine("");
-     
-     Console.WriteLine("Assigned Task:");
      Console.WriteLine("");
-     Console.WriteLine(Task[0]+members[0]);
-     Console.WriteLine(Task[1]+members[1]);
-     Console.WriteLine(Task[2]+members[2]);
+     Console.WriteLine("Add Task:");
      
-     }
-     
+     Task[0] = Console.ReadLine();
+     Task[1] = Console.ReadLine();
+     Task[2] = Console.ReadLine();
    
      
      
+     Console.WriteLine("");
      
-     
-     
-        } 
+     Console.WriteLine("Assigned Task:");
+     Console.WriteLine("");
+     Console.WriteLine(Task[0]+"--"+members[0]);
+     Console.WriteLine(Task[1]+"--"+members[1]);
+     Console.WriteLine(Task[2]+"--"+members[2]);
+           
+           
+           
+           
+           
+        }
     }
 }
